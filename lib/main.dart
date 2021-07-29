5782d321a879ae5508cec92fdcdd6f4a1a0bcc12
DimaDontsov <Dimayahoo@mail.ru>
2021-07-30 00:45:25 +0300

    В верхнюю часть меню добавлены аватар, имя и почта

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,40 @@ class _GalleryPageState extends State<GalleryPage> {
       appBar: AppBar(
         title: Text('Галерея', style: TextStyle(color: Colors.white,),),
         backgroundColor: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      drawer: Drawer(
+
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/menu_icon.png'),
+                    radius: 20,
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 20),),
+                  Text('Дмитрий Донцов', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black,),),
+                  Padding(padding: EdgeInsets.only(top: 10),),
+                  Text('test@mail.ru', style: TextStyle(fontSize:15, color: Colors.grey,),),
+                  Padding(padding: EdgeInsets.only(top: 20),),
+                  // Text('Count: ${_count}', style: TextStyle(fontSize:20, color: Colors.black,),),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
