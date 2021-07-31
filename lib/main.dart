7c5ba1d96663567d9b59f7886fc139653e742156
DimaDontsov <Dimayahoo@mail.ru>
2021-07-31 10:40:33 +0300

    На странице "Обо мне" добавлен header и меню

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,58 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Обо мне', style: TextStyle(color: Colors.white,),),
+        backgroundColor: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/menu_icon.png'),
+                    radius: 20,
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 20),),
+                  Text('Дмитрий Донцов', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black,),),
+                  Padding(padding: EdgeInsets.only(top: 10),),
+                  Text('test@mail.ru', style: TextStyle(fontSize:15, color: Colors.grey,),),
+                  Padding(padding: EdgeInsets.only(top: 20),),
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text("Галерея", style: TextStyle(color: Colors.black,),),
+              leading: Icon(Icons.folder, color: Colors.black,),
+              onTap: (){
+                Navigator.pushReplacementNamed(context, '/gallery');
+              },
+            ),
+            ListTile(
+                title: Text("Обо мне", style: TextStyle(color: Colors.black,),),
+                leading: Icon(Icons.people, color: Colors.black,),
+                onTap: (){
+                  Navigator.pushReplacementNamed(context, '/about');
+                }
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
