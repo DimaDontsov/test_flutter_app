a226b60afe67ef54c59f44990dfb19c8e062acce
DimaDontsov <Dimayahoo@mail.ru>
2021-07-31 14:02:25 +0300

    Добавлен собственный GridView

--- lib/pages/galleryPage.dart
+++ lib/pages/galleryPage.dart
@@ -3,6 +3,34 @@ import 'package:test_flutter_app/widgets/myAppBar.dart';
 import 'package:test_flutter_app/widgets/myDrawer.dart';
 
 
+class MyGridList extends StatelessWidget {
+  const MyGridList({Key? key}) : super(key: key);
+
+  List<Image> images(BuildContext context) {
+    return [
+      Image.asset('assets/menu_icon.png'),
+      Image.asset('assets/menu_icon.png'),
+      Image.asset('assets/menu_icon.png'),
+      Image.asset('assets/menu_icon.png'),
+      Image.asset('assets/menu_icon.png'),
+      Image.asset('assets/menu_icon.png'),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  GridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 8,
+      crossAxisSpacing: 8,
+      padding: const EdgeInsets.all(8),
+      childAspectRatio: 1,
+      children: images(context).toList(),
+    );
+  }
+}
+
+
 class GalleryPage extends StatefulWidget {
   GalleryPage({Key? key}) : super(key: key);
 
@@ -17,6 +45,11 @@ class _GalleryPageState extends State<GalleryPage> {
     return Scaffold(
       appBar: MyAppBar(titleText: 'Галерея',),
       drawer: MyDrawer(),
+      body: Scrollbar(
+        child: MyGridList(),
+      ),
     );
   }
 }
+
+
