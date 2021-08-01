eeea7790bb6cc8f1cac6880b0b7b494f4aebac98
DimaDontsov <Dimayahoo@mail.ru>
2021-08-01 17:52:14 +0300

    Изменена страница "Обо мне"

--- lib/pages/aboutPage.dart
+++ lib/pages/aboutPage.dart
@@ -16,6 +16,46 @@ class _AboutPageState extends State<AboutPage> {
     return Scaffold(
       appBar: MyAppBar(titleText: 'Обо мне',),
       drawer: MyDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: Text('Меня зовут Дмитрий Донцов.', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                'Мне 24 года. Окончил бакалавриат (2015-2019) и магистратуру (2019-2021) '
+                    'по направлению "Программная инженерия" в ВолгГТУ.',
+                style: TextStyle(fontSize: 20, ),),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                'С программированием познакомился на уроках информатики в 9 классе. '
+                    'Нам давали задачки, которые нужно было закодить на Pascal ABC. '
+                    'В 10 классе рассказали и показали, что такое HTML и как на нём верстать.',
+                style: TextStyle(fontSize: 20, ),),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                'На текущей работе занимаюсь вёрсткой лэндингов / сайтов, а также их дальнейшей поддержкой. '
+                    'Front - ванильные HTML, CSS, JS. '
+                    'Back - PHP, MySQL, CMS Bitrix, CMS Netcat, CMS Modx.',
+                style: TextStyle(fontSize: 20, ),),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                'Хобби: музыка (игра на гитаре), волейбол, история кино, настольные игры.',
+                style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
